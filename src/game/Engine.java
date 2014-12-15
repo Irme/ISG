98c636165aa83f50092d765542488ce74fd8eeb7
Irme <Irme_G@hotmail.com>
2014-12-15 22:49:19 +0000

    hjk

--- src/game/Engine.java
+++ src/game/Engine.java
@@ -79,7 +79,7 @@ public class Engine {
 				} else {
 					int [] temp = new int [4];
 					int [][] state = board.clone(Board.board);
-					temp = m.MinimaxDecision(true,state, Board.player, 4);
+					temp = ab.AlphaBetaSearch(true, state, Board.player, 5);
 					System.out.println("Moving from " + temp[0]  +","  +temp[1] + " to " + temp[2] + "," + temp[3]);
 					Board.Moving(temp[0], temp[1], temp[2], temp[3]);
 				}
@@ -117,8 +117,8 @@ public class Engine {
 				}else {
 					int [] temp = new int [4];
 					int [][] state = board.clone(Board.board);
-					temp = ab.AlphaBetaSearch(true, state, Board.player, 5);
-					//temp = m.MinimaxDecision(true,state, Board.player, 4);
+					
+					temp = m.MinimaxDecision(true,state, Board.player, 4);
 					System.out.println("Moving from " + temp[0]  +","  +temp[1] + " to " + temp[2] + "," + temp[3]);
 					Board.Moving(temp[0], temp[1], temp[2], temp[3]);
 				}
